f1820ad930852c3a0983941542886a5e3e967372
petr <krkabol@gmail.com>
2025-06-23 18:33:35 +0200

    fix typo

--- src/Enum/JacqAppNetwork.php
+++ src/Enum/JacqAppNetwork.php
@@ -5,7 +5,7 @@ namespace JACQ\Enum;
 enum JacqAppNetwork: string
 {
     case Output = 'https://jacq.org';
-    case Services = 'https://services.jacq.org';
+    case Services = 'https://services.jacq.org/jacq-services/rest';
     case Api = 'https://api.jacq.org';
 
 }
