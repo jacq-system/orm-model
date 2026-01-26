fe64a5b7e8cadacbd487f3ea02ebc8735052e32c
Petr Novotný <krkabol@gmail.com>
2026-01-26 08:43:53 +0100

    search for country-insittutio

---
+++ src/Repository/Herbarinput/CountryRepository.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace JACQ\Repository\Herbarinput;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use JACQ\Entity\Jacq\Herbarinput\Country;
+use JACQ\Entity\Jacq\Herbarinput\Institution;
+
+
+class CountryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Country::class);
+    }
+
+    public function findWithInstitutions(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->innerJoin(
+                Institution::class,
+                'i',
+                'WITH',
+                'i.country = c'
+            )
+            ->addSelect('i')
+            ->distinct()
+            ->orderBy('c.name', 'ASC')
+            ->addOrderBy('i.name', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+
+
+}
